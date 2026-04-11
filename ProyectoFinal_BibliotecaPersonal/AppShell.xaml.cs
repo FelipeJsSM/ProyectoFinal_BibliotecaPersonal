4b307dfe0156503b6959f3bbb35de51e62432f12
Felipe Solórzano <94859615+FelipeJsSM@users.noreply.github.com>
2026-04-11 10:58:34 -0400

    feat: Diseño UI completo, estilos globales y vistas base

--- ProyectoFinal_BibliotecaPersonal/AppShell.xaml.cs
+++ ProyectoFinal_BibliotecaPersonal/AppShell.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace ProyectoFinal_BibliotecaPersonal
+﻿using ProyectoFinal_BibliotecaPersonal.Views;
+
+namespace ProyectoFinal_BibliotecaPersonal
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(BookDetailPage), typeof(BookDetailPage));
+            Routing.RegisterRoute(nameof(AddBookPage), typeof(AddBookPage));
         }
     }
 }
