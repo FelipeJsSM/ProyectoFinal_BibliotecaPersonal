da255ba7fe838ff5e911c21a7e782f84f42651f7
RadiFrancoPT <radyrolandofranco23@gmail.com>
2026-04-18 10:14:55 -0400

    Configuracion del HttpClient

--- ProyectoFinal_BibliotecaPersonal/MauiProgram.cs
+++ ProyectoFinal_BibliotecaPersonal/MauiProgram.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;   
+using ProyectoFinal_BibliotecaPersonal.Services;  
+using ProyectoFinal_BibliotecaPersonal.Views;     
 
 namespace ProyectoFinal_BibliotecaPersonal
 {
@@ -15,6 +18,11 @@ namespace ProyectoFinal_BibliotecaPersonal
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.Services.AddHttpClient<BookApiService>(client => {
+                client.BaseAddress = new Uri("https://www.googleapis.com/books/v1/");
+                client.Timeout = TimeSpan.FromSeconds(30);
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+            });
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
